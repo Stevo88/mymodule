2951e3448f2a4d6668bbc2c865ec415aac8e9e71
Steven <stevecross88@gmail.com>
2022-12-04 23:38:53 +0100

    add function

--- mymodule.go
+++ mymodule.go
@@ -2,6 +2,8 @@ package mypackage
 
 import (
 	"fmt"
+	"os"
+	"bufio"
 )
 
 func Hello() string {
@@ -11,4 +13,24 @@ func Hello() string {
 func HelloAgain() string {
 	fmt.Println("and again")
 	return "Hello, Again!"
+}
+
+func makeArrayFromFile(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var wordArray []string
+	for scanner.Scan() {
+		wordArray = append(wordArray, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return wordArray, err
 }
